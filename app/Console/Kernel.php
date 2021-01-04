e53b9cac82677c4a810f4d2dacb46434f7b857d9
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2021-01-04 22:42:16 +0100

    new

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,7 +26,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        
         $schedule->command('miniute:udpate')->everyMinute();
+
+
+
     }
 
     /**
