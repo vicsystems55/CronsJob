0d2f949fe499dba43d9ab4141ac26123e5222556
vicSystesms <46149752+vicsystems55@users.noreply.github.com>
2021-01-04 15:38:26 +0100

    new codebase

---
+++ app/Console/Commands/MinuteUpdate.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Listing;
+
+class MinuteUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'minute:udpate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+
+  
+
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        
+        return 0;
+
+
+    }
+}
